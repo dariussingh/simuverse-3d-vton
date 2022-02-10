1588c0020ee1154c0e5ed369ee5b34867332995c
dariussingh <darius2121.ds@gmail.com>
2022-02-10 17:31:06 +0530

    VTON

---
+++ inference.py
@@ -0,0 +1,52 @@
+import cv2
+import matplotlib.pyplot as plt
+import os
+from os.path import exists, join, basename, splitext
+import open3d as o3d 
+
+if __name__=='__main__':
+    # create clothing mask
+    img = cv2.imread('./m3d-vton/input_data/cloth/cloth@1=cloth_front.jpg')
+    img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img,200, 255, cv2.THRESH_BINARY_INV)
+    cv2.imwrite('./m3d-vton/input_data/cloth-mask/cloth@1=cloth_front_mask.jpg', thresh)
+    
+    # creating image parse
+    !cp -r './m3d-vton/input_data/image/person@1=person_whole_front.png' './2D-Human-Parsing/demo_imgs'
+    !cd 2D-Human-Parsing
+    with open('./inference/img_list.txt', 'w') as f:
+        f.write('./inference/person@1=person_whole_front.png')
+    with open('./demo_imgs/img_list.txt', 'w') as f:
+        f.write('./demo_imgs/person@1=person_whole_front.png')
+    !cd inference
+    !bash demo.sh
+    !cd ..
+    !cd ..
+    !cp -r './2D-Human-Parsing/parsing_result/train_parsing/demo_imgs/person@1=person_whole_front_label.png' './m3d-vton/input_data/image-parse' 
+    
+    # creating pose keypoints
+    !cp -r './m3d-vton/input_data/image/person@1=person_whole_front.png' './openpose/examples/media'
+    !cd openpose
+    ! ./build/examples/openpose/openpose.bin --video /content/openpose/examples/media/person@1=person_whole_front.png --display 0 --render_pose 0 --face --hand --write_json output_json_folder/
+    !cd ..
+    !cp -r './openpose/output_json_folder/person@1=person_whole_front_000000000000_keypoints.json' './m3d-vton/input_data/pose/person@1=person_whole_front_keypoints.json'
+    
+    # running inference
+    !cd m3d-vton
+    !python util/data_preprocessing.py --MPV3D_root input_data
+    !python test.py --model MTM --name MTM --dataroot input_data --datalist test_pairs --results_dir results
+    !python test.py --model DRM --name DRM --dataroot input_data --datalist test_pairs --results_dir results
+    !python test.py --model TFM --name TFM --dataroot input_data --datalist test_pairs --results_dir results
+    !python rgbd2pcd.py --parse_root input_data/image-parse
+    
+    # post processing pcd
+    !mkdir ./results/aligned/final_pcd
+    # load point cloud
+    pcd = o3d.io.read_point_cloud('./results/aligned/pcd/test_pairs/person@1=person.ply')
+    # normal estimation
+    pcd.estimate_normals()
+    pcd.orient_normals_consistent_tangent_plane(100)
+    # poisson surface reconstruction
+    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
+    # save mesh
+    o3d.io.write_triangle_mesh('./results/aligned/final_pcd/person@1=person.ply', mesh)
---
+++ setup.py
@@ -0,0 +1,45 @@
+!pip install -r requirements.txt
+
+import cv2
+import matplotlib.pyplot as plt
+import os
+from os.path import exists, join, basename, splitext
+import open3d as o3d 
+
+if __name__=='__main__':
+    # m3d-vton
+    !git clone https://github.com/fyviezhao/m3d-vton
+    !cd m3d-vton/pretrained
+    !gdown https://drive.google.com/uc?id=1wpN-yfa1_MbezqkcLfVspa2jVTJV7bjN
+    !unzip -o M3D-VTON.zip 
+    !rm M3D-VTON.zip
+    !mv latest_net_DRM.pth ./aligned/DRM/
+    !mv latest_net_MTM.pth ./aligned/MTM/
+    !mv latest_net_TFM.pth ./aligned/TFM/
+    !cd ..
+    !cd ..
+
+    # 2D human parsing
+    !git clone https://github.com/fyviezhao/2D-Human-Parsing
+    !cd 2D-Human-Parsing/pretrained
+    !gdown https://drive.google.com/uc?id=1cTYhbKbtrPfREVI_EkredmIq5WaT5GY6
+    !cd ..
+    !cd ..
+
+    # openpose
+    git_repo_url = 'https://github.com/CMU-Perceptual-Computing-Lab/openpose.git'
+    project_name = splitext(basename(git_repo_url))[0]
+    if not exists(project_name):
+      # see: https://github.com/CMU-Perceptual-Computing-Lab/openpose/issues/949
+      # install new CMake becaue of CUDA10
+      !wget -q https://cmake.org/files/v3.13/cmake-3.13.0-Linux-x86_64.tar.gz
+      !tar xfz cmake-3.13.0-Linux-x86_64.tar.gz --strip-components=1 -C /usr/local
+      # clone openpose
+      !git clone -q --depth 1 $git_repo_url
+      !sed -i 's/execute_process(COMMAND git checkout master WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}\/3rdparty\/caffe)/execute_process(COMMAND git checkout f019d0dfe86f49d1140961f8c7dec22130c83154 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}\/3rdparty\/caffe)/g' openpose/CMakeLists.txt
+      # install system dependencies
+      !apt-get -qq install -y libatlas-base-dev libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler libgflags-dev libgoogle-glog-dev liblmdb-dev opencl-headers ocl-icd-opencl-dev libviennacl-dev
+      # install python dependencies
+      !pip install -q youtube-dl
+      # build openpose
+      !cd openpose && rm -rf build || true && mkdir build && cd build && cmake .. && make -j`nproc`
