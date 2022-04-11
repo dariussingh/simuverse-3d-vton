e3ee3f085e739fc908b590081bca49ee66a6ab18
dariussingh <darius2121.ds@gmail.com>
2022-04-11 18:14:34 +0530

    fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if __name__=='__main__':
     os.chdir("..")
     with open('./inference/demo.sh', 'w') as f:
         f.write("""CUDA_VISIBLE_DEVICES=0 \\
-python inference_acc.py \\
+python3 inference_acc.py \\
 --loadmodel '../pretrained/deeplabv3plus-xception-vocNov14_20-51-38_epoch-89.pth' \\
 --img_list '../demo_imgs/img_list.txt' \\
 --output_dir '../parsing_result'""")
