4427508b07f92590fe7e6f71550e05774a8c5c47
dariussingh <75756866+dariussingh@users.noreply.github.com>
2022-05-13 20:55:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ python3 inference_acc.py \\
       # see: https://github.com/CMU-Perceptual-Computing-Lab/openpose/issues/949
       # install new CMake becaue of CUDA10
       os.system("wget -q https://cmake.org/files/v3.13/cmake-3.13.0-Linux-x86_64.tar.gz")
-      os.system("sudo tar xfz cmake-3.13.0-Linux-x86_64.tar.gz --strip-components=1 -C /usr/local")
+      os.system("tar xfz cmake-3.13.0-Linux-x86_64.tar.gz --strip-components=1 -C /usr/local")
       # clone openpose
       os.system("git clone -q --depth 1 'https://github.com/CMU-Perceptual-Computing-Lab/openpose.git'")
       os.system("sudo sed -i 's/execute_process(COMMAND git checkout master WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}\/3rdparty\/caffe)/execute_process(COMMAND git checkout f019d0dfe86f49d1140961f8c7dec22130c83154 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}\/3rdparty\/caffe)/g' openpose/CMakeLists.txt")
